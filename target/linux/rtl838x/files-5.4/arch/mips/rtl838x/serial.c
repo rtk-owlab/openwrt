58aa6f0d8c68bf551969f5adc5a4d82840671858
bkobl <25039569+bkobi@users.noreply.github.com>
2020-05-27 14:12:45 +0200

    Fixing UART for TP-Link T2500G

--- target/linux/rtl838x/files-5.4/arch/mips/rtl838x/serial.c
+++ target/linux/rtl838x/files-5.4/arch/mips/rtl838x/serial.c
@@ -22,14 +22,12 @@
 #include "mach-rtl838x.h"
 
 extern char arcs_cmdline[];
-extern struct clk cpu_clk;
 
 int __init rtl838x_serial_init(void)
 {
 #ifdef CONFIG_SERIAL_8250
 	int ret;
 	struct uart_port p;
-	u32 cpu_freq = clk_get_rate(&cpu_clk);
 	int baud = 0;
 	char parity = '\0', bits = '\0', flow = '\0';
 	char *s, *n;
@@ -53,26 +51,27 @@ int __init rtl838x_serial_init(void)
 			flow = 'r';
 	}
 
-	if (baud == 0)
+	if (baud == 0) {
 		baud = 38400;
+		pr_warn("Using default baud rate: %d\n", baud);
+	}
 	if (parity != 'n' && parity != 'o' && parity != 'e')
 		parity = 'n';
 	if (bits != '7' && bits != '8')
 		bits = '8';
 
-	printk("rtl838x_serial_init: cpu_freq: %d baud %d\n", cpu_freq, baud);
 	memset(&p, 0, sizeof(p));
-		
+
 	p.type = PORT_16550A;
 	p.membase = (unsigned char *) RTL838X_UART0_BASE;
 	p.irq = RTL838X_UART0_EXT_IRQ;
-	p.uartclk = ((cpu_freq << 1) /5) - baud * 24;
-	p.flags = UPF_SKIP_TEST | UPF_LOW_LATENCY | UPF_SPD_CUST;
+	p.uartclk = SYSTEM_FREQ - (24 * baud);
+	p.flags = UPF_SKIP_TEST | UPF_LOW_LATENCY | UPF_FIXED_TYPE;
 	p.iotype = UPIO_MEM;
 	p.regshift = 2;
 	p.fifosize = 1;
-	p.custom_divisor = ((cpu_freq << 1) /5) / (baud * 16) - 1;
-	
+	p.custom_divisor = SYSTEM_FREQ / (baud * 16) - 1;
+
 	/* Call early_serial_setup() here, to set up 8250 console driver */
 	if (early_serial_setup(&p) != 0) {
 		ret = 1;
